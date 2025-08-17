0e7e08e99294dc2c74ced75009f3122476caca4c
lemon_juice_1 <1582017385@qq.com>
2025-08-17 10:26:46 +0800

    配置文件初始化现在被拆分到单独的类中

---
+++ src/main/java/io/lemonjuice/flandre_bot/ConfigInit.java
@@ -0,0 +1,39 @@
+package io.lemonjuice.flandre_bot;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Log4j2
+public class ConfigInit {
+    public static void initConfig() {
+        File configFolder = new File("./config");
+        if(!configFolder.exists()) {
+            configFolder.mkdir();
+        }
+        try {
+            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            Resource[] configs = resolver.getResources("classpath:export/config/*");
+            for (Resource cfg : configs) {
+                File cfgFile = new File(configFolder.getPath() + "/" + cfg.getFilename());
+                if(!cfgFile.exists()) {
+                    releaseConfig(cfg.getFilename());
+                }
+            }
+        } catch (IOException e) {
+            log.error("初始化配置文件失败！", e);
+        }
+    }
+
+    private static void releaseConfig(String fileName) throws IOException {
+        try (InputStream input = Start.class.getClassLoader().getResourceAsStream("export/config/" + fileName);
+             FileOutputStream output = new FileOutputStream("./config/" + fileName)) {
+            output.write(input.readAllBytes());
+        }
+    }
+}
--- src/main/java/io/lemonjuice/flandre_bot/Start.java
+++ src/main/java/io/lemonjuice/flandre_bot/Start.java
@@ -8,14 +8,7 @@ import io.lemonjuice.flandre_bot.reference.NetworkRefs;
 import io.lemonjuice.flandre_bot.utils.NicknameManager;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Component;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 @Component
 @Log4j2
@@ -23,7 +16,7 @@ public class Start implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        initConfig();
+        ConfigInit.initConfig();
         ResourceLoadHandler.getInstance().loadResources();
         WSClientCore.startWatchDog();
         if(!WSClientCore.connect(NetworkRefs.WS_URL, NetworkRefs.WS_TOKEN)) {
@@ -31,31 +24,5 @@ public class Start implements CommandLineRunner {
         }
         SQLCore.connect(NetworkRefs.SQL_URL, NetworkRefs.SQL_USERNAME, NetworkRefs.SQL_PASSWORD);
         NicknameManager.init();
-    }
-
-    private static void initConfig() {
-        File configFolder = new File("./config");
-        if(!configFolder.exists()) {
-            configFolder.mkdir();
-        }
-        try {
-            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-            Resource[] configs = resolver.getResources("classpath:export/config/*");
-            for (Resource cfg : configs) {
-                File cfgFile = new File(configFolder.getPath() + "/" + cfg.getFilename());
-                if(!cfgFile.exists()) {
-                    releaseConfig(cfg.getFilename());
-                }
-            }
-        } catch (IOException e) {
-            log.error("初始化配置文件失败！", e);
-        }
-    }
-
-    private static void releaseConfig(String fileName) throws IOException {
-        try (InputStream input = Start.class.getClassLoader().getResourceAsStream("export/config/" + fileName);
-             FileOutputStream output = new FileOutputStream("./config/" + fileName)) {
-            output.write(input.readAllBytes());
-        }
     }
 }
