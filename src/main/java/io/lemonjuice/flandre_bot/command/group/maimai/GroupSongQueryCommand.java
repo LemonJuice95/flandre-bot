187633a309d59dc57af243a72b6a009c213b0679
lemon_juice_1 <1582017385@qq.com>
2025-08-19 02:49:32 +0800

    修改别名查询多个结果时的回复

--- src/main/java/io/lemonjuice/flandre_bot/command/group/maimai/GroupSongQueryCommand.java
+++ src/main/java/io/lemonjuice/flandre_bot/command/group/maimai/GroupSongQueryCommand.java
@@ -77,7 +77,12 @@ public class GroupSongQueryCommand extends GroupCommandRunner {
         } else {
             StringBuilder reply = new StringBuilder("好像有不止一首歌叫这个名字呢\n芙兰帮你列出来了哦~\n\n");
             for(Song s : songs) {
-                reply.append("id").append(s.id).append(".    ").append(s.title).append("\n");
+                reply.append("id");
+                reply.append(s.id);
+                reply.append(".");
+                reply.append(" ".repeat(9 - String.valueOf(s.id).length()));
+                reply.append(s.title);
+                reply.append("\n");
             }
             SendingUtils.sendGroupText(command.groupId, CQCodeUtils.reply(command.messageId) + reply.toString().trim());
         }
