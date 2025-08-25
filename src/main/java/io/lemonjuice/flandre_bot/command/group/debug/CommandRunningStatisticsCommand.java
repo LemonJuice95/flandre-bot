8a2e84f4f134b52da12f90e3846c0df3c7690c49
lemon_juice_1 <1582017385@qq.com>
2025-08-25 23:56:52 +0800

    修复正则

--- src/main/java/io/lemonjuice/flandre_bot/command/group/debug/CommandRunningStatisticsCommand.java
+++ src/main/java/io/lemonjuice/flandre_bot/command/group/debug/CommandRunningStatisticsCommand.java
@@ -14,7 +14,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class CommandRunningStatisticsCommand extends GroupCommandRunner {
-    private static final String commandPattern = "^\\[CQ:at,qq=%d]\\s*/命令统计.+";
+    private static final String commandPattern = "^\\[CQ:at,qq=%d]\\s*/命令统计.*";
 
     private final Pattern argsPattern = Pattern.compile("\\s+-(a(ll)?|h(istory)?|s(orted)?)");
 
