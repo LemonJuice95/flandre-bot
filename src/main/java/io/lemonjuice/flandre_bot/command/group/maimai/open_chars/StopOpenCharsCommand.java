67e20660fb4ababe06b14d65abd2cf044d9366e2
lemon_juice_1 <1582017385@qq.com>
2025-09-01 22:40:09 +0800

    结束后将会发送标准答案

--- src/main/java/io/lemonjuice/flandre_bot/command/group/maimai/open_chars/StopOpenCharsCommand.java
+++ src/main/java/io/lemonjuice/flandre_bot/command/group/maimai/open_chars/StopOpenCharsCommand.java
@@ -1,11 +1,13 @@
 package io.lemonjuice.flandre_bot.command.group.maimai.open_chars;
 
+import io.lemonjuice.flan_mai_plugin.model.Song;
 import io.lemonjuice.flandre_bot.command.group.GroupCommandRunner;
 import io.lemonjuice.flandre_bot.command.group.permission.IPermissionLevel;
 import io.lemonjuice.flandre_bot.command.group.permission.PermissionLevel;
 import io.lemonjuice.flandre_bot.message.Message;
 import io.lemonjuice.flandre_bot.utils.SendingUtils;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -32,8 +34,18 @@ public class StopOpenCharsCommand extends GroupCommandRunner {
 
     @Override
     public void apply() {
-        if(OpenCharsManager.stopProcess(this.command.groupId)) {
+        if(OpenCharsManager.hasProcess(this.command.groupId)) {
             SendingUtils.sendGroupText(this.command.groupId, "诶？不玩了吗？好吧……");
+            StringBuilder answer = new StringBuilder("下面是这次的答案：\n");
+            List<Song> songs = OpenCharsManager.getProcess(this.command.groupId).getSongs();
+            for(int i = 0; i < songs.size(); i++) {
+                answer.append("[");
+                answer.append(i);
+                answer.append("]  ");
+                answer.append(songs.get(i).title);
+                answer.append("\n");
+            }
+            SendingUtils.sendGroupText(this.command.groupId, answer.toString().trim());
         } else {
             SendingUtils.sendGroupText(this.command.groupId, "群里没有正在进行的开字母哦~");
         }
