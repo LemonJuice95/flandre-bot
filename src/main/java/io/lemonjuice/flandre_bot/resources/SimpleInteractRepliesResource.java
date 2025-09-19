afe6ce8de473d542307f9c36599e781751490d6a
lemon_juice_1 <1582017385@qq.com>
2025-09-19 21:13:44 +0800

    加一张图片

--- src/main/java/io/lemonjuice/flandre_bot/resources/SimpleInteractRepliesResource.java
+++ src/main/java/io/lemonjuice/flandre_bot/resources/SimpleInteractRepliesResource.java
@@ -1,22 +1,27 @@
 package io.lemonjuice.flandre_bot.resources;
 
 import io.lemonjuice.flandre_bot_framework.resource.Resource;
+import io.lemonjuice.flandre_bot_framework.utils.CQCode;
 import lombok.extern.log4j.Log4j2;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.StreamSupport;
 
 @Log4j2
 public class SimpleInteractRepliesResource extends Resource<Map<String, List<String>>> {
+    private static final Pattern image_pattern = Pattern.compile("<(local|net)Image:(\\S+)>");
 
     public SimpleInteractRepliesResource() {
         super("assets/interact/simple.json", Collections.unmodifiableMap(new HashMap<>()));
@@ -34,6 +39,7 @@ public class SimpleInteractRepliesResource extends Resource<Map<String, List<Str
                 JSONArray repliesJson = action.getJSONArray("replies");
                 List<String> replies = StreamSupport.stream(repliesJson.spliterator(), false)
                         .map(String::valueOf)
+                        .map(this::parseImage)
                         .toList();
                 StreamSupport.stream(msgJson.spliterator(), false)
                         .map(String::valueOf)
@@ -45,5 +51,25 @@ public class SimpleInteractRepliesResource extends Resource<Map<String, List<Str
             }
         }
         return Collections.unmodifiableMap(result);
+    }
+
+    private String parseImage(String str) {
+        Matcher matcher = image_pattern.matcher(str);
+        while (matcher.find()) {
+            String type = matcher.group(1);
+            String path = matcher.group(2);
+            if(type.equals("local")) {
+                File imageFile = new File(path);
+                if (imageFile.exists()) {
+                    str = matcher.replaceFirst(CQCode.image("file:///" + imageFile.getAbsolutePath()));
+                } else {
+                    log.warn("找不到简单回复文本中的本地图片: {}", path);
+                }
+            } else {
+                str = matcher.replaceFirst(CQCode.image(path));
+            }
+            matcher = image_pattern.matcher(str);
+        }
+        return str;
     }
 }
